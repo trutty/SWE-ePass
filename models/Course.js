7b592ca1e30956a1c8497f17c339b1262ea6fb4e
marc <mharnos@gmail.com>
2013-03-14 12:58:42 +0100

    fixed Course.js

--- models/Course.js
+++ models/Course.js
@@ -1,38 +1,42 @@
-var mongoose = require('mongoose')
+module.exports = function(User){
+
+  var mongoose = require('mongoose')
   , Validator = require('validator').Validator;
 
-var Schema = mongoose.Schema
+  var Schema = mongoose.Schema
   , ObjectId = Schema.ObjectId
   , val = new Validator();
 
-Validator.prototype.error = function(msg) {
-    return false;
-};
+  Validator.prototype.error = function(msg) {
+      return false;
+  };
 
-var CourseSchema = new Schema({
+  var CourseSchema = new Schema({
 
-  name: {
-  	type: String,
-  	required: true
-  },
+    name: {
+      type: String,
+      required: true
+    },
 
-  year: {
-    type: Number,
-    required: true
-  },
+    year: {
+      type: Number,
+      required: true
+    },
 
-  //Sommersemester/Wintersemester
-  term: {
-    type: String,
-    'enum': ['winter', 'summer'],
-    default: 'winter',
-    required: true
-  },
+    //Sommersemester/Wintersemester
+    term: {
+      type: String,
+      'enum': ['winter', 'summer'],
+      default: 'winter',
+      required: true
+    },
 
-  userlist: {
-    type: [User]
-  }
+    userlist: {
+      type: [User]
+    }
 
-});
+  });
 
-module.exports = mongoose.model('Course', CourseSchema);
+  return mongoose.model('Course', CourseSchema);
+  
+}
