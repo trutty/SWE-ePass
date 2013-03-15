60177730b2b40dc7e4436ebca08d6dc87ba0ff53
marc <mharnos@gmail.com>
2013-03-15 16:07:16 +0100

    <INSERT COMMIT MESSAGE PLOX>

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,100 @@
+var async = require('async');
+var alphabet = [
+	
+	['a', 'ä'],
+	['b'],
+	['c'],
+	['d'],
+	['e'],
+	['f'],
+	['g'],
+	['h'],
+	['i'],
+	['j'],
+	['k'],
+	['l'],
+	['m'],
+	['n'],
+	['o', 'ö'],
+	['p'],
+	['q'],
+	['r'],
+	['s'],
+	['t'],
+	['u', 'ü'],
+	['v'],
+	['w'],
+	['x'],
+	['y'],
+	['z']
+
+];
+
 module.exports = function(app, User, Course, Exam){
 
 	app.get('/api/students/:type/:attr', function (req, res) {
 		if(req.params.type == 'az') {
-			var position = 0;
 			var splitting = req.params.attr;
 
-			User.find({}, function (err, docs) {
-				console.log(docs);
-				res.json(docs);
+			//build request alphabet
+			var splittedAlphabet = [];
+			for (var i = 0; i < alphabet.length; i++) {
+				
+				if(i % splitting == 0) {
+					
+					if(splittedAlphabet.length > 0) {
+						splittedAlphabet[splittedAlphabet.length - 1].push(alphabet[i - 1]);
+					}
+
+					splittedAlphabet[splittedAlphabet.length] = [alphabet[i]];
+
+				}
+
+			};
+			splittedAlphabet[splittedAlphabet.length - 1].push(alphabet[alphabet.length - 1]);
+
+			var splittedUsers = [];
+			async.forEach(splittedAlphabet, function (item, callback) {
+				
+				var re = new RegExp('^([' + item[0][0] + '-' + item[1][0] + '])', 'i');
+
+				User.find({ lastname: re }, function (err, docs) {
+					var cat = {};
+					cat.categoryName = item[0][0] + '-' + item[1][0];
+					cat.categoryItems = docs;
+
+					splittedUsers.push(cat);
+					callback();
+				});
+
+			}, function (err) {
+				res.json(splittedUsers);
 			});
-			
 		}
 
 		if(req.params.type == 'course') {
 			var courseId = req.params.attr;
 
-			console.log(courseId);
+			/*Course.findOne({ _id: courseId }, function (err, doc) {
+				
+				console.log(err);
+
+				var courseUsers = [];
+				
+				var course = {};
+				course.categoryName = doc.name;
+				course.categoryItems = doc.userlist;
+
+				courseUsers.push(course);
+				res.json(courseUsers);
+			});
+			*/
+
+			var cat = {};
+			cat.categoryName = "Kurs TINF11A";
+			cat.categoryItems = [];
+
+			res.json([cat]);
 		}
 	});
 
