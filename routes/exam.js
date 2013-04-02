92ece6c8257b47a34676b99d450736eab2ce462f
Christian Schulz <crs.s@gmx.net>
2013-04-02 11:44:43 +0200

    added exam.js

---
+++ routes/exam.js
@@ -0,0 +1,182 @@
+
+/*
+ * Routes for exam creation, manipulation and view
+ */
+
+module.exports = function (app, User, Course, Criteria, Exam, async){
+	// exam
+	// exam overview
+	app.get('/exam', function (req, res) {
+
+		var exams = [];
+
+		Exam
+			.find({})
+			.populate('course', 'userlist')
+			.or([{'assessor': req.user.id}, {'user': req.user.id}, {'course.userlist': req.user.id}])
+			.exec(function (error, docs) {
+				res.render('exam/view/exam', {
+					title: 'Exams',
+					message: req.flash('error'),
+					exam: docs
+				});
+			});
+
+	});
+
+	// exam create
+	var createEditExam = function( req, res, selectedExam ) {
+
+		var courses 	= []
+		  , assessors 	= []
+		  , tutors 		= []
+		  , exam 		= null;
+
+		async.series([
+			function (callback) {
+				if(selectedExam) {
+					Exam
+						.findById(selectedExam)
+						.populate('criteria course assessor')
+						.exec(function (err, docsExam) {
+						exam = docsExam;
+						callback(err);
+
+						console.log(exam);
+						console.log('-------');
+						console.log('-------');
+						console.log('-------');
+					});
+				}
+
+				callback();
+			},
+			function (callback) {
+				Course.find({}, function (err, docsCourse) {
+					courses = docsCourse;
+					callback(err);
+				});
+			},
+			function (callback) {
+				User.find( {role: 'assessor'}, function (err, docsAssessor) {
+					
+					docsAssessor.forEach(function(item){
+						var assessor 		= {};
+						assessor.name 		= item.name;
+						assessor.firstname	= item.firstname;
+						assessor.lastname 	= item.lastname;
+						assessor.id 		= item.id;
+
+						assessors.push(assessor);
+					});
+
+					callback(err);
+				});
+			},
+			function (callback) {
+				User.find({role: 'tutor'}, function (err, docsTutor) {	
+
+					docsTutor.forEach(function(item){
+						var tutor 		= {};
+						tutor.name 		= item.name;
+						tutor.firstname	= item.firstname;
+						tutor.lastname 	= item.lastname;
+						tutor._id 		= item.id;
+
+						tutors.push(tutor);
+					});
+
+					callback(err);
+
+				});
+
+			}
+
+		], function(error) {
+
+			console.log("Exam: %s", exam);
+
+			res.render('exam/manage/new', {
+				title: exam == null ? 'New Exam' : 'Update Exam',
+	    		message: req.flash('error'),
+	    		tutors: tutors,
+	    		registered: req.user,
+				courses: courses,
+				assessors: assessors.concat(tutors),
+				exam: exam
+			});
+
+		});
+
+	};
+
+	app.get('/exam/new', function(req, res) {
+		createEditExam(req, res, null);
+	});
+
+	app.get('/exam/edit/:selectedExam', function(req, res) {
+		createEditExam(req, res, req.params.selectedExam);
+	});
+
+/*	// exam edit
+	app.get('/exam/edit/:selectedExam', function(req, res) {
+
+		Exam
+			.findById(req.params.selectedExam)
+			.populate('user')
+			.populate('assessor')
+			.populate('course')
+			.exec(function (error, docs) {
+
+				res.render('exam/manage/edit', {
+					title: 'Edit Exam',
+					message: req.flash('error'),
+					exam: docs
+				});
+
+			});
+
+	});*/
+
+	// exam detail view
+	app.get('/exam/view/:selectedExam', function(req, res) {
+
+		Exam
+			.findById(req.params.selectedExam)
+			.populate('user')
+			.populate('assessor')
+			.populate('course')
+			.exec(function (error, docs) {
+
+				res.render('exam/view/details', {
+					title: 'Exam Details',
+					message: req.flash('error'),
+					exam: docs
+				});
+
+			});
+		
+	});
+
+	app.post('/exam/new', function(req, res) {
+
+		var examBody 		= req.body;
+		examBody.criteria 	= req.body.criteria;
+		examBody.assessor 	= req.body.assessor;
+		examBody.user 		= req.body.tutor[0];
+		examBody.course 	= req.body.course;
+
+		var exam 			= new Exam(examBody);
+
+		exam.save(function (err) {
+			if(!err) {
+				res.redirect('/exam');
+			} else {
+				console.log(examBody);
+				console.log(err);
+			}
+		});
+		
+	});
+
+}
