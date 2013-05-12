fefe0ab839df04d577f3354432da505a69dff08c
chris <trutty3@gmail.com>
2013-05-12 19:47:59 +0200

    corrected server db

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.configure('development', function(){
 });
 
 //small test for git
-mongoose.connect("mongodb://localhost:27017/epass", function(err){
+mongoose.connect("mongodb://moep_mongoadmin:idHemRog@localhost:20869/admin", function(err){
     if(err) console.log(err);
 });
 
